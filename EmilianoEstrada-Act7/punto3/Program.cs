1ea5b50aab79914ca5efd8fa82bad6a00247671b
emiliano-estra <147885831+emiliano-estra@users.noreply.github.com>
2026-04-10 09:23:42 -0300

    Add files via upload
    Practica de vectores: Estructura de datos y tamaño

---
+++ EmilianoEstrada-Act7/punto3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto3
+{
+    internal class Puntuacion
+    {
+        private int[] puntos;
+        public void carga()
+        {
+            puntos = new int[6];
+            string linea;
+            for (int i = 1; i < 7; i++)
+            {
+                Console.Write("ingrese la puntuacion de la partida numero "+i+": ");
+                linea = Console.ReadLine();
+                puntos[i-1] = int.Parse(linea);
+            }
+        }
+        public void imprimir()
+        {
+            int max,min,mas_500,promedio;
+            max=maxima_puntuacion();
+            min=minima_puntuacion();
+            mas_500 = mayor_500();
+            promedio = calcular_promedio();
+            Console.WriteLine("la puntuacion maxima es: "+max);
+            Console.WriteLine("la puntuacion minima es: " + min);
+            Console.WriteLine("la cantidad de puntuacion mayor a 500 es: " + mas_500);
+            Console.WriteLine("el promedio es: " + promedio);
+        }
+        public int maxima_puntuacion()
+        {
+            int maximo;
+            maximo = puntos[0];
+            for (int i = 1; i < 6; i++)
+            {
+                if (maximo < puntos[i])
+                {
+                    maximo = puntos[i];
+                }
+            }
+            return maximo;
+        }
+        public int minima_puntuacion()
+        {
+            int minimo;
+            minimo = puntos[0];
+            for (int i = 1; i < 6; i++)
+            {
+                if (minimo > puntos[i])
+                {
+                    minimo = puntos[i];
+                }
+            }
+            return minimo;
+        }
+        public int mayor_500()
+        {
+            int mayor;
+            mayor = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                if (puntos[i]>500)
+                {
+                    mayor++;
+                }
+            }
+            return mayor;
+        }
+        public int calcular_promedio()
+        {
+            int promedio,suma;
+            promedio = 0;
+            suma = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                suma = suma + puntos[i];
+                if (i == 5) {
+                    promedio = suma / 6;
+                }
+            }
+            return promedio;
+        }
+        static void Main(string[] args)
+        {
+            /*Pedir al usuario que ingrese las puntuaciones obtenidas en 6 partidas seguidas de
+            un videojuego. El programa debe:
+             Mostrar la puntuación más alta y la más baja.
+             Calcular el promedio de puntuación.
+             Contar cuántas veces superó los 500 puntos.*/
+            Puntuacion punto = new Puntuacion();
+            punto.carga();
+            punto.imprimir();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EmilianoEstrada-Act7/punto4/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto4
+{
+    internal class mayor_menor
+    {
+        private int[] orden;
+        public void cargar()
+        {
+            orden = new int[10];
+            string linea;
+            for (int i = 0; i < orden.Length; i++)
+            {
+                Console.Write("ingrese un valor al vector: ");
+                linea = Console.ReadLine();
+                orden[i]=int.Parse(linea);
+            }
+        }
+        public void imprimir()
+        {
+            bool mayor_menor,menor_mayor;
+            mayor_menor = false;
+            menor_mayor = false;
+            mayor_menor = orden_mayor_menor();
+            menor_mayor = orden_menor_mayor();
+            if (mayor_menor) {
+                Console.WriteLine("esta ordenado de mayor a menor");
+            }
+            else if(menor_mayor)
+            {
+                Console.WriteLine("esta ordenado de menor a mayor");
+            }
+            else { Console.Write("esta mal"); }
+        }
+        public bool orden_mayor_menor()
+        {
+            bool es_mayor;
+            int cant;
+            es_mayor = false;
+            cant = 0;
+            for (int i = 0; i < orden.Length; i++) {
+                if (i < orden.Length-1)
+                {
+                    if (orden[i] > orden[i + 1 ]) {
+                        cant++;
+               
+                    }
+                }
+            }
+            if (cant == 9) { 
+            es_mayor=true;
+            }
+            return es_mayor;
+        }
+        public bool orden_menor_mayor()
+        {
+            bool es_menor;
+            int cant;
+            es_menor = false;
+            cant = 0;
+            for (int i = 0; i < orden.Length; i++)
+            {
+                if ( i < orden.Length-1)
+                {
+                    if (orden[i] < orden[i + 1])
+                    {
+                        cant++;
+                    }
+                }
+
+            }
+            if (cant == 9)
+            {
+                es_menor = true;
+            }
+            return es_menor;
+        }
+        static void Main(string[] args)
+        {
+            /*Cargar un vector de 10 elementos y verificar posteriormente si el mismo está
+            ordenado de menor a mayor y de mayor a menor.*/
+            mayor_menor ordenado=new mayor_menor();
+            ordenado.cargar();
+            ordenado.imprimir();
+            Console.ReadKey();
+        }
+    }
+}
