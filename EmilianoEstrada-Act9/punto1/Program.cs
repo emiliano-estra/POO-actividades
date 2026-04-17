93cb13109f500b34d80045b910bec08850e47891
emiliano-estra <147885831+emiliano-estra@users.noreply.github.com>
2026-04-17 12:04:02 -0300

    Add files via upload
    Práctica de Vectores paralelos, mayor y menor elemento, ordenamiento - Parte II

---
+++ EmilianoEstrada-Act9/punto1/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto1
+{
+    internal class Notas_alum
+    {
+        private string[] alumno;
+        private int[] nota;
+        private int[] posicionMax;
+        private int[] posicionMin;
+        public void cargar()
+        {
+            alumno = new string[6];
+            nota= new int[6];
+            string linea;
+            for(int i = 0; i < alumno.Length; i++)
+            {
+                Console.Write("ingrese el nombre del alumno: ");
+                linea = Console.ReadLine();
+                alumno[i] = linea;
+                Console.Write("ingrese la nota del alumno: ");
+                linea= Console.ReadLine();
+                nota[i] =int.Parse(linea);
+            }
+        }
+        public void cantMax_numMax()
+        {
+            int cant = 0, max = 0, u = 0;
+            for (int i = 0; i < alumno.Length; i++)
+            {
+                if (max < nota[i])
+                {
+                    max = nota[i];
+                    
+                }
+            }
+            for (int f = 0; f < alumno.Length; f++)
+            {
+                if (max == nota[f])
+                {
+                    cant++;
+                    
+                }
+            }
+            posicionMax = new int[cant];
+            for (int p = 0; p < nota.Length; p++)
+            {
+                if (max == nota[p])
+                {
+                    posicionMax[u] = p;
+                    
+                    u++;
+
+                }
+            }
+        }
+        public void cantMin_numMin()
+        {
+            int cant2 = 0, min = 10, u = 0;
+            for (int i = 0; i < alumno.Length; i++)
+            {
+                if (min > nota[i])
+                {
+                    min = nota[i];
+                }
+            }
+            for (int f = 0; f < alumno.Length; f++)
+            {
+                if (min == nota[f])
+                {
+                    cant2++;
+                }
+            }
+            posicionMin = new int[cant2];
+            for (int p = 0; p < nota.Length; p++)
+            {
+                if (min == nota[p])
+                {
+                    posicionMin[u] = p;
+                    u++;
+                }
+            }
+        }
+        public void imprimir()
+        {
+            string alumnoMax="",alumnoMin="";
+            for(int i = 0; i < posicionMax.Length; i++)
+            {
+                alumnoMax+= alumno[posicionMax[i]]+" ";
+            }
+            for (int i = 0; i < posicionMin.Length; i++)
+            {
+                alumnoMin += alumno[posicionMin[i]] + " ";
+            }
+            if (posicionMax.Length > 2 && posicionMin.Length>2)
+            {
+                Console.WriteLine("los alumnos " + alumnoMax + " son los que tiene la nota maxima con un " + nota[posicionMax[0]]);
+                Console.WriteLine("los alumnos " + alumnoMin + " son los que tiene la nota minima con un " + nota[posicionMin[0]]);
+            }
+            else if(posicionMax.Length>2 && posicionMin.Length < 2)
+            {
+                Console.WriteLine("los alumnos " + alumnoMax + " son los que tiene la nota maxima con un " + nota[posicionMax[0]]);
+                Console.WriteLine("el alumno " + alumnoMin + " es el que tiene la nota minima con un " + nota[posicionMin[0]]);
+            }
+            else if (posicionMax.Length < 2 && posicionMin.Length > 2)
+            {
+                Console.WriteLine("el alumno " + alumnoMax + " es el que tiene la nota maxima con un " + nota[posicionMax[0]]);
+                Console.WriteLine("los alumnos " + alumnoMin + " son los que tiene la nota minima con un " + nota[posicionMin[0]]);
+            }
+            else
+            {
+                Console.WriteLine("el alumno " + alumnoMax + " es el que tiene la nota maxima con un " + nota[posicionMax[0]]);
+                Console.WriteLine("el alumno " + alumnoMin + " es el que tiene la nota minima con un " + nota[posicionMin[0]]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            /*Se desea desarrollar un programa que permita registrar los nombres y las
+            calificaciones de 6 estudiantes. Luego de cargar los datos, se debe mostrar el
+            nombre del estudiante con la nota más alta, junto con su nota. Al igual que el
+            estudiante con la nota más baja. Informar si hay estudiantes con la misma nota
+            máxima o mínima.*/
+            Notas_alum alumno=new Notas_alum();
+            alumno.cargar();
+            alumno.cantMax_numMax();
+            alumno.cantMin_numMin();
+            alumno.imprimir();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EmilianoEstrada-Act9/punto2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto2
+{
+    internal class venta
+    {
+        private string[] vendedor;
+        private int[] ventatotal;
+        public void cargar()
+        {
+            vendedor=new string[5];
+            ventatotal=new int[5];
+            string linea;
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write("ingrese el nombre del vendedor: ");
+                linea = Console.ReadLine();
+                vendedor[i] = linea;
+                Console.Write("ingrese las ventas totales del vendedor: ");
+                linea=Console.ReadLine();
+                ventatotal[i]=int.Parse(linea);
+            }
+           
+        }
+        public void orden()
+        {
+            int aux;
+            string aux_nombre;
+            for (int i = 0; i < vendedor.Length; i++) {
+                for (int j = 0; j < vendedor.Length; j++) {
+                    if (j != vendedor.Length - 1)
+                    {
+                        if (ventatotal[i] < ventatotal[i+1])
+                        {
+                            aux = ventatotal[j];
+                            ventatotal[j] = ventatotal[j + 1];
+                            ventatotal[j + 1] = aux;
+                            aux_nombre = vendedor[j];
+                            vendedor[j] = vendedor[j + 1];
+                            vendedor[j + 1] = aux_nombre;
+                        }
+                    }
+                }
+            }
+        }
+        public void imprimir()
+        {
+            Console.Write("la lista de vendedores de mayor a menor:");
+           for (int i = 0;i < vendedor.Length; i++)
+            {
+                Console.WriteLine(vendedor[i]+" " + ventatotal[i]);
+            }
+            Console.WriteLine("el que vendio menos es:" + vendedor[4]+" con " + ventatotal[4]);
+        }
+        static void Main(string[] args)
+        {
+            /*Una empresa registra los nombres de sus 5 vendedores y el total de ventas
+            realizadas por cada uno en un mes. Cargar los nombres y ventas en dos
+            vectores paralelos, ordenar los datos de mayor a menor según las ventas,
+            imprimir la lista ordenada con nombre y monto de la venta, e informar quien fue
+            el que menos vendió de los 5 empleados.*/
+            venta vn = new venta();
+            vn.cargar();
+            vn.orden();
+            vn.imprimir();
+            Console.ReadKey();
+        }
+    }
+}
