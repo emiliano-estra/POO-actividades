2bedf8abbe4588c1ee3be7cc7ba4795522fb1414
emiliano-estra <147885831+emiliano-estra@users.noreply.github.com>
2026-05-15 11:44:25 -0300

    Add files via upload
    Práctica de matrices: Con vectores paralelos e irregulares o dentadas

--- EmilianoEstrada-Act12/punto2/Program.cs
+++ EmilianoEstrada-Act12/punto2/Program.cs
@@ -6,10 +6,116 @@ using System.Threading.Tasks;
 
 namespace punto2
 {
-    internal class Program
+    internal class cine
     {
-        static void Main(string[] args)
+        string[] sala;
+        int[][] cines;
+        public void cargaSala()
         {
+            sala = new string[4];
+            cines = new int[4][];
+            cines[0] = new int[10];
+            cines[1] = new int[15];
+            cines[2] = new int[8];
+            cines[3] = new int[12];
+            for (int i = 0; i < 4; i++)
+            {
+                sala[i] = "sala" + i;
+                Console.WriteLine(sala[i]);
+                for (int j = 1; j < cines[i].Length+1; j++)
+                {
+                    Console.Write(cines[i] [j-1]);
+                    if (j == cines[i].Length+ / 2)
+                    {
+                        Console.Write("\n");
+                    }
+                    
+                }
+                Console.WriteLine();
+            }
+
+        }
+        public void ventaDeEntradas()
+        {
+            string linea;
+            int cantVoletos=0;
+            int salaL = 0;
+            int voletosSala = 0;
+            int fila = 0;
+            int columna = 0;
+            int edad = 0;
+            Console.Write("ingrese cuantos voletos quiere: ");
+            linea=Console.ReadLine();
+            cantVoletos=int.Parse(linea);
+            for (int i = 0; i < cantVoletos; i++) {
+                Console.Write("ingrese en que sala va a comprar su voleto: ");
+                linea = Console.ReadLine();
+                salaL=int.Parse(linea);
+                Console.Write("cuantos voletos quiere para la sala" + sala[salaL] +": ");
+                linea = Console.ReadLine();
+                voletosSala = int.Parse(linea);
+                for (int g = 0; g < voletosSala; g++) {
+                    Console.Write("ingrese la fila: ");
+                    linea = Console.ReadLine();
+                    fila=int.Parse(linea);
+                    Console.Write("ingrese la columna: ");
+                    linea = Console.ReadLine();
+                    columna = int.Parse(linea);
+                    for (int j = 0; j < 1; j++) {
+                        Console.Write("ingrese la edad de la persona: ");
+                        linea = Console.ReadLine();
+                        edad = int.Parse(linea);
+                        if (fila == 2)
+                        {
+                            columna = columna + cines[salaL].Length/2;
+                        }
+                        cines[salaL][columna]=edad;
+                    }
+                }
+                i=i+voletosSala-1;
+            }
+
+        }
+        public void calcularPromedio(){
+            int cantMenores = 0;
+            int suma = 0;
+            int cantPersonas = 0;
+            double promedio = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < cines[i].Length; j++) {
+                        if (cines[i][j] < 18&& cines[i][j]>0)
+                        {
+                            cantMenores++;
+                        }
+                        suma = suma + cines[i][j];
+                        cantPersonas++;
+                }
+                Console.Write("en la sala " + i + " hay " + cantMenores + " menores de edad.");
+               cantMenores = 0;
+            }
+            promedio = suma / cantPersonas;
+            Console.Write("el promedio de edad es " + promedio);
+        }
+        static void Main(string[] args)
+        {/*Un cine tiene 4 salas con diferentes capacidades de espectadores (la Sala 1 tiene 10
+        asientos, la Sala 2 tiene 15, la Sala 3 tiene 8 y la Sala 4 tiene 12).
+        ● Definir una matriz irregular de 4 filas para representar los asientos.
+        ● Métodos:
+        1. Inicializar la matriz con los tamaños de las salas mencionadas (sin
+        intervención del operador).
+        2. Crear un método de Venta de Entradas que permita cargar la edad del
+        espectador en un asiento específico (fila y columna).
+        3. Imprimir el mapa de ocupación de las salas indicando la edad del espectador
+        en cada asiento.
+        4. Calcular cuántos menores de edad (menos de 18 años) hay en cada sala.
+        5. Informar cuál es el promedio de edad de los espectadores de todo el
+        complejo.*/
+            cine persona = new cine();
+            persona.cargaSala();
+            persona.ventaDeEntradas();
+            persona.calcularPromedio();
+            Console.ReadKey();
         }
     }
 }
---
+++ EmilianoEstrada-Act12/punto3/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace punto3
+{
+    internal class chef
+    {
+        string[] alumno;
+        int[][] plato;
+        public void cargar()
+        {
+            alumno = new string[3];
+            plato = new int[3][];
+            plato[0] = new int[2];
+            plato[1] = new int[4];
+            plato[2] = new int[3];
+            string linea;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("ingrese el nombre del alumno: ");
+                linea = Console.ReadLine();
+                alumno[i] = linea;
+
+                for (int j = 0; j < plato[i].Length; j++)
+                {
+                    Console.Write("ingrese la calificacion del alumno en el plato " + j + " (ingrese de 0 a 100): ");
+                    linea = Console.ReadLine();
+                    if (int.Parse(linea) > 0 && int.Parse(linea) < 100)
+                    {
+                        plato[i][j] = int.Parse(linea);
+                    }
+                    else
+                    {
+                        Console.Write("ingreso mal el numero");
+                        j--;
+                    }
+                }
+            }
+        }
+        public void promedio()
+        {
+            double sumaPlato = 0;
+            double promedio = 0;
+            double mayorPromedio = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < plato[i].Length; j++)
+                {
+                    sumaPlato = sumaPlato + plato[i][j];
+                }
+                promedio = sumaPlato / plato[i].Length;
+                sumaPlato = 0;
+                if (mayorPromedio < promedio)
+                {
+                    mayorPromedio = promedio;
+                }
+                Console.Write("el alumno " + alumno[i] + "esta ");
+                if (promedio >= 70)
+                {
+                    Console.Write("aprobado");
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write("desaprobado");
+                    Console.WriteLine();
+                }
+                if (i == 2)
+                {
+                    Console.Write("el promedio mas alto es:"+mayorPromedio);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {/*Un chef instructor evalúa a 3 alumnos en un examen final. Cada alumno debe presentar
+        una cantidad distinta de platos (uno presenta 2 platos, otro 4 y otro 3).
+        ● Definir un vector para los nombres de los alumnos.
+        ● Definir una matriz irregular para cargar el puntaje obtenido (0 a 100) en cada plato
+        presentado.
+        ● Métodos:
+        1. Cargar nombres y definir el tamaño de las filas según la cantidad de platos
+        de cada alumno.
+        2. Cargar los puntajes de cada plato validando que estén entre 0 y 100.
+        3. Mostrar el listado de alumnos y el puntaje de cada uno de sus platos.
+        4. Calcular el puntaje promedio de cada alumno e informar si está Aprobado
+        (promedio >= 70) o Reprobado.
+        5. Determinar quién obtuvo el puntaje individual más alto en un solo plato (el
+        valor máximo de la matriz).*/
+            chef puntos=new chef();
+            puntos.cargar();
+            puntos.promedio();
+            Console.ReadKey();
+        }
+    }
+}
