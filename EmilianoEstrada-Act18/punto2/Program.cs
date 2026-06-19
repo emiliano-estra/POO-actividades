7c3f675e60e1b079aae4c988e7c1eb36ae3b4673
emiliano-estra <147885831+emiliano-estra@users.noreply.github.com>
2026-06-19 01:54:05 -0300

    Add files via upload

--- EmilianoEstrada-Act18/punto2/Program.cs
+++ EmilianoEstrada-Act18/punto2/Program.cs
@@ -54,12 +54,74 @@ namespace punto2
     class Habitatauatico : Criaturamarina
     {
         public Criaturamarina[] criatura;
-        public void carga()
+        public void Carga()
         {
+            string linea;
+            bool v = true;
             criatura=new Criaturamarina[3];
             for(int i = 0; i < 3; i++) {
                 criatura[i]=new Criaturamarina();
+                Console.WriteLine("ingrese la especie maritima: ");
+                criatura[i].Especie = Console.ReadLine();
+                Console.WriteLine("ingrese la profundidad optima: ");
+                linea = Console.ReadLine();
+                if (int.Parse(linea) <= 0) {
+                    while (v)
+                    {
+                        Console.WriteLine("ingrese la profundidad optima (que sea mayor a cero): ");
+                        linea= Console.ReadLine();
+                        if (int.Parse(linea) > 0)
+                        {
+                            criatura[i].Profundidadoptima = int.Parse(linea);
+                            v = false;
+                        }
+                    }
+                }
+                else
+                {
+                    criatura[i].Profundidadoptima = int.Parse(linea);
+                }
+                Console.WriteLine("ingrese el nivel de sanidad: ");
+                linea= Console.ReadLine();
+                criatura[i].Niversalinidad=int.Parse(linea);
             }
+        }
+        public void Imprimir()
+        {
+            int[] orden, indice;
+            orden = new int[3];
+            indice = new int[3];
+            orden[0] = criatura[0].Profundidadoptima;
+            orden[1] = criatura[1].Profundidadoptima;
+            orden[2] = criatura[2].Profundidadoptima; 
+            Array.Sort(orden);
+            for (int j = 0; j < orden.Length; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (orden[j] == criatura[i].Profundidadoptima)
+                    {
+                        indice[j] = i;
+                    }
+                }
+            }
+            for(int t = 0;t < 3; t++)
+            {
+                Console.WriteLine("la especie " + criatura[indice[t]].Especie +" tiene una profundidad optima de " + orden[t]+" con un nivel de salinidad de " + criatura[indice[t]].Niversalinidad);
+            } 
+        }
+        public void Salinidad()
+        {
+            int mayor = criatura[0].Niversalinidad, indice=0;
+            for (int j = 0; j < 3; j++)
+            {
+                if (mayor < criatura[j].Niversalinidad)
+                {
+                    mayor=criatura[j].Niversalinidad;
+                    indice=j;
+                }
+            }
+            Console.WriteLine("la especie que necesita mayor salinidad es " + criatura[indice].Especie+" con "+mayor);
         }
     }
     class Prueba
@@ -77,6 +139,10 @@ namespace punto2
         criaturas y otro método para mostrar todas las criaturas ordenadas de menor a mayor en base
         a su profundidad óptima. Además, el programa debe informar la especie que requiere el mayor
         nivel de salinidad para sobrevivir.*/
+            Habitatauatico h = new Habitatauatico();
+            h.Carga();
+            h.Imprimir();
+            h.Salinidad();
             Console.ReadKey();
         }
     }
