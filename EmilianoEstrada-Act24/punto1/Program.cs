01770803d53b1e7a890ff52d0567656ac469d507
emiliano-estra <147885831+emiliano-estra@users.noreply.github.com>
2026-08-13 10:01:56 -0300

    Add files via upload
    Práctica de Repaso - Extra

---
+++ EmilianoEstrada-Act24/punto1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto1
+{
+    class Plato
+    {
+        private string nombreplato;
+        private double precio;
+        public string Nombreplato
+        {
+            set
+            {
+                nombreplato = value;
+            }
+            get
+            {
+                return nombreplato;
+            }
+        }
+        public double Precio
+        {
+            set
+            {
+                precio = value;
+            }
+            get
+            {
+                return precio;
+            }
+        }
+        public Plato(string nom,double pre) {
+            Nombreplato=nom;
+            Precio = pre;
+        }
+    }
+    class Gestioncomandas
+    {
+        List<Plato> g = new List<Plato>();
+        public void Agregarplato()
+        {
+            string linea, nombre;
+            double precio;
+            Console.WriteLine("ingrese el nombre del plato: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("ingrese el precio del plato: ");
+            linea = Console.ReadLine();
+            precio=double.Parse(linea);
+            Plato p=new Plato(nombre, precio);
+            g.Add(p);
+        }
+        public void Mostrarcomanda()
+        {
+            foreach (Plato i in g)
+            {
+                Console.WriteLine("los platos que hay agregados a la comanda "+i.Nombreplato);
+                
+            }
+            Console.WriteLine("platos total " + g.Count);
+        }
+        public void Cancelarplato()
+        {
+            string nombre;
+            Console.WriteLine("ingrese el plato que quiere cancelar del pedido: ");
+            nombre = Console.ReadLine();
+            Plato t = g.FirstOrDefault(l => l.Nombreplato == nombre);
+            g.Remove(t);
+        }
+        public void Calculartotalmesa()
+        {
+            double total=0;
+            foreach (Plato t in g)
+            {
+                total=t.Precio + total;
+            }
+            Console.WriteLine("el costo de la mesa es de "+total);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*Un restaurante automatizado procesa la comanda de una mesa para controlar la
+        preparación y el cobro de los platos pedidos.
+         Crear la clase Plato que contenga como atributos privados: nombrePlato
+        (string) y precio (double). Definir sus propiedades correspondientes y un
+        constructor que reciba nom y pre.
+         Crear la clase GestionComandas que administre una lista de objetos List.
+         Métodos en GestionComandas:
+        o AgregarPlato():Solicitar por teclado los datos de un plato y
+        agregarlo a la lista utilizando .Add().
+        o MostrarComanda(): Listar todos los platos agregados hasta el
+        momento junto a la cantidad total de ítems pedidos utilizando la
+        propiedad .Count.
+        o CalcularTotalMesa(): Calcular y mostrar en pantalla el monto
+        total a cobrar sumando los precios de la lista.
+        o CancelarPlato(): Solicitar al usuario el nombre de un plato y, si
+        se encuentra en la lista, removerlo mediante .Remove() para
+        actualizar la comanda.*/
+            Gestioncomandas h=new Gestioncomandas();
+            for(int i=0; i<3; i++)
+            {
+                h.Agregarplato();
+            }
+            h.Mostrarcomanda();
+            h.Cancelarplato();
+            h.Mostrarcomanda();
+            h.Calculartotalmesa();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EmilianoEstrada-Act24/punto2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*Una farmacia controla el nivel de stock de sus medicamentos para evitar el
+        desabastecimiento de insumos esenciales.
+         Crear la clase Medicamento que contenga los atributos privados: nombre
+        (string) y stock (int). Definir sus propiedades correspondientes. Su
+        constructor debe recibir nom y stk.
+         Crear la clase ControlFarmacia que administre una lista de objetos List.
+         Métodos en ControlFarmacia:
+        o Un constructor que cargue por teclado una lista inicial de 4
+        medicamentos ingresando sus nombres y stock.
+        o ListarStock(): Mostrar la lista de medicamentos en pantalla.
+
+        o RemoverAgotados(): Recorrer la lista y remover por completo de
+        la colección a todos aquellos medicamentos cuyo stock sea igual a 0.
+        o MostrarMedicamentosDisponibles(): Imprimir la lista
+        actualizada y la cantidad de productos disponibles en el inventario
+        utilizando la propiedad .Count.*/
+        }
+    }
+}
